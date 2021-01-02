8ac53f5160f6cc772513ec96cef19b53945fa45e
siya-008 <74820530+siya-008@users.noreply.github.com>
2021-01-02 18:38:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,109 +1,85 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Constraint=Matter.Constraint;
+const Constraint = Matter.Constraint;
 
+var engine, world;
+var box1;
+var BackgroundImg,platform;
+var bird, slingShot;
 
-//const Constraint=Matter.Constraint
-var  circle1,ground;	
-var world,sling;
+function preload () {
+    
+BackgroundImg = loadImage("bg3.jpg");
 
-
-function setup() {
-	createCanvas(1200, 600);
-	rectMode(CENTER);
-
-
-	engine = Engine.create();
-	world = engine.world;
-	
- ground=createSprite(400,400,230,20)
-ground=new Ground(400,400,230,20)
- 	World.add(world, ground);
-
- 	box1=new Box(310,370,30,40)
-
-box2=new Box(340,370,30,40)
-box3=new Box(370,370,30,40)
-box4=new Box(400,370,30,40)
-box5=new Box(430,370,30,40)
-box6=new Box(460,370,30,40)
-box7=new Box(490,370,30,40)
-box8=new Box(340,330,30,40)
-box9=new Box(370,330,30,40)
-box10=new Box(400,330,30,40)
-box11=new Box(430,330,30,40)
-box12=new Box(460,330,30,40)
-box13=new Box(370,290,30,40)
-box14=new Box(400,290,30,40)
-box15=new Box(430,290,30,40)
-box16=new Box(400,250,30,40)
-circle1=new Circle(100,200,20)
-slingShot =new SlingShot (circle1.body,{x:130,y:200})
-
-
-	
-	
-	sling =new SlingShot (circle1.body,{x:130,y:200})
-
-Engine.run(engine);
-	
-  
 }
 
 
-function draw() {
-	background("blue");
-Engine.update(engine);
-  //rectMode(CENTER);
-  
+function setup(){
+    createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
 
- 
- 
- ground=createSprite(400,400,230,20)
-  circle1.display();
-  sling.display();
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display();
-  box5.display();
-  box6.display();
-  box7.display();
-  box8.display();
-  box9.display();
-  box10.display();
-  box11.display();
-  box12.display();
-  box13.display();
-  box14.display();
-  box15.display();
-  box16.display();
-  ground.display();
+    ground = new Ground(600,height,1200,20);
+    
+    sidewall = new Ground (1180,200,20,1200)
+    
 
-  drawSprites();
+    box1 = new Box(760,150,110,80);
+    box2 = new Box(855,150,110,80);
 
- 
-}
- 
+    log1 = new Log(805,260,300, PI/2);
+
+    box3 = new Box(692,255,50,40);
+    box4 = new Box(920,255,50,40);
    
-  function mouseDragged(){
-	Matter.Body.setPosition(circle1.body,{x:mouseX,y:mouseY})
-	
-	
-	}
- 	function mouseReleased (){
-	sling.fly();
-	
-  }
+    box5 = new Box (810,255,60,40);
 
-  function keyPressed(){
-	if(keyCode=== 32){
-	  
-	 // Matter.Body.setPosition(circle.body,{x:130,y:200});
-	  sling.attach(circle1.body);
-	}
-  }
+    box6 = new Box(810,100,100,100);
+    
+    bird = new Poligon(100,100,200,100);
+
+
+    slingshot = new SlingShot(bird.body,{x:200, y:100});
+}
+
+function draw(){
+    background(BackgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    sidewall.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    
+   
+
+    box5.display();
+    box6.display();
+
+    bird.display();
+
+    slingshot.display(); 
+    
+    
+
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
+}
